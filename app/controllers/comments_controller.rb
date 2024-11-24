1dd1f6c0e8fbd035031fa2a77514a9ea8c5a2a74
Piampiya <piampiyasaengperm@gmail.com>
2024-11-24 14:04:17 +0700

    Part 4

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,10 +7,8 @@ class CommentsController < ApplicationController
     @comment.user = current_user
 
     if @comment.save
-      flash[:notice] = 'Comment has been created'
       redirect_to post_path(@post)
     else
-      flash[:alert] = 'Comment has not been created'
       redirect_to post_path(@post)
     end
   end
@@ -19,6 +17,18 @@ class CommentsController < ApplicationController
     @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
+  end
+
+  def update
+    @comment = @post.comments.find(params[:id])
+
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to post_url(@post), notice: 'Comment has been updated' }
+      else
+        format.html { redirect_to post_url(@post), alert: 'Comment was not updated!' }
+      end
+    end
   end
 
   private
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "https://cdn.jsdelivr.net/npm/@hotwired/turbo@7.2.0/dist/turbo.es2017-umd.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://cdn.jsdelivr.net/npm/@hotwired/stimulus@3.2.2/dist/stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.js"
+pin "trix", to: "https://cdn.jsdelivr.net/npm/trix@2.1.8/dist/trix.umd.min.js"
+pin "@rails/actiontext", to: "https://cdn.jsdelivr.net/npm/@rails/actiontext@7.1.0/app/assets/javascripts/actiontext.min.js"
