b0854f67f56c44f790b19bf3181e4d78709e56ee
Piampiya <piampiyasaengperm@gmail.com>
2024-11-30 23:51:58 +0700

    validate age

--- app/models/personal_info.rb
+++ app/models/personal_info.rb
@@ -1,7 +1,7 @@
 class PersonalInfo < ApplicationRecord
   belongs_to :user, inverse_of: :personal_info
   validates :gender, presence: true
-  validates :age, presence: true
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :country, presence: true
   validates :goal, presence: true
 end
